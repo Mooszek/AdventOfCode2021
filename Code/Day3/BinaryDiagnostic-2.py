7576e2ccafc079677f11636fba606bb8ae39e9e3
Mooszek <Mooszek@gmail.com>
2021-12-04 19:11:13 +0100

    Most Frequent to be fixed

--- Code/Day3/BinaryDiagnostic-2.py
+++ Code/Day3/BinaryDiagnostic-2.py
@@ -22,7 +22,9 @@ def left(s, amount):
 def most_frequent(lista2, column):  # counter is for current column
     common_list = []
     for item in transpose(lista2):
-        common,  = Counter(item).most_common()
+        common = Counter(item).most_common()
+        # if common[0][1] == common[1][1]:
+        #    print("q")
         common_list.append(common)
     return common_list[column][0][0]  # returns most frequent number
 
@@ -33,17 +35,18 @@ cipher = ""  # do tego bedziemy dodawac kolejne bity
 
 # print(lines)
 # print(transpose(lines))
-#print(most_frequent(lines, 0))
+# print(most_frequent(lines, 0))
 
 for item in range(0, 5):
     column = item
     cipher += most_frequent(lines, column)
     for item in lines:
-        if left(item, column+1) == cipher:
-            lines2 = []
-            lines2.append(item)
+        if left(item, column+1) != cipher:
+            lines.remove(item)
+            #lines2 = []
+            # lines2.append(item)
 #    print(lines2)
 
 
 # print(lines2)
-# print(cipher)
+print(cipher)
